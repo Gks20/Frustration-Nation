9044a9dc83846c26429d11070292c5ac7d58baa0
dwrigley1 <dwrigley1@ivytech.edu>
2025-09-27 16:11:38 -0400

    added frontend templates & URLs

--- Project Red/frontend/views.py
+++ Project Red/frontend/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "frontend/base.html")
+
+def examples(request):
+    return render(request, "frontend/ui_examples.html")
+
+'''
+def function(request):
+    return render(request, "frontend/filename.html")
+'''
--- Project Red/project_red/urls.py
+++ Project Red/project_red/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from frontend import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",views.index, name="index"), # project home page
+    path("ui_examples/", views.examples, name="ui_examples"),
 ]
