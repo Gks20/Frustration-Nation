b1cd7763e624cd22a78e240990c20ef004dbc13f
Dakota Wrigley <130004968+dwrigley1@users.noreply.github.com>
2025-10-18 11:30:18 -0400

    Update urls.py

--- Project Red/store/urls.py
+++ Project Red/store/urls.py
@@ -1,8 +1,10 @@
+# import library
 from django.urls import path
 from . import views
 
 app_name = 'store'
 
+# URL to be used to navigate to the store
 urlpatterns = [
     path('', views.storefront, name='storefront'),
 ]
